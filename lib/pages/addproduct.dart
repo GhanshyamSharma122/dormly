396de24b9f60c11b91f61ca7e32eaf79a3ab32eb
shyam poudel <poudelshyam844@gmail.com>
2025-02-24 14:41:09 +0530

    good

--- lib/pages/addproduct.dart
+++ lib/pages/addproduct.dart
@@ -4,8 +4,14 @@ class AddProduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("add product"),
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Add Product"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Text("ram ram satya hai"),
+      ),
     );
   }
 }
