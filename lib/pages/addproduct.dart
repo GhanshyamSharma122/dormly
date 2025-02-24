b7a2f5a8253d82441fe8041eaaa2ecaffdc98a1b
shyam poudel <poudelshyam844@gmail.com>
2025-02-24 15:48:28 +0530

    added the ui

--- lib/pages/addproduct.dart
+++ lib/pages/addproduct.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
-class AddProduct extends StatelessWidget {
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+class AddProduct extends StatefulWidget {
   const AddProduct({super.key});
 
   @override
+  State<AddProduct> createState() => _AddProductState();
+}
+
+class _AddProductState extends State<AddProduct> {
+  final _formKey=GlobalKey<FormState>();
+  final TextEditingController _nameController=TextEditingController();
+  final TextEditingController _priceController=TextEditingController();
+  final TextEditingController _descriptionController=TextEditingController();
+  final TextEditingController _contactNumberController=TextEditingController();
+  final TextEditingController _contactDetailsController=TextEditingController();
+  File? _image;
+  final ImagePicker _picker=ImagePicker();
+  Future<void> _pickImage() async{
+    final pickedFile=await _picker.pickImage(source: ImageSource.gallery);
+    if(pickedFile!=null){
+      setState(() {
+        _image=File(pickedFile.path);
+      });
+    }
+  }
+  void _submitForm(){
+    if(_formKey.currentState!.validate()){
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Product added Successfully!"))
+      );
+    }
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
-        title: Text("Add Product"),
+      appBar: AppBar(
+        title: const Text("Add Product"),
         backgroundColor: Colors.red,
       ),
-      body: Center(
-        child: Text("ram ram satya hai"),
+      body: Padding(padding: EdgeInsets.all(16.0),
+      child:Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _nameController,
+                decoration: InputDecoration(labelText: "Product Name"),
+                validator: (value)=>value!.isEmpty?"Enter product name":null,
+              )
+            ],
+          ),
+        ),
+      )
       ),
     );
   }
